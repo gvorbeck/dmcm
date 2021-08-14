2dcdf4e81810155eeb64b34cb34a40e92ca9ec97
Garrett Vorbeck <me@iamgarrett.com>
2021-08-14 14:15:16 -0400

    player list and area notes

--- src/components/location-page-layout/location-page-layout.js
+++ src/components/location-page-layout/location-page-layout.js
@@ -5,6 +5,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../layout/layout';
 import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import showdown from 'showdown';
 
 export const query = graphql`
 query ($id: String, $pid: String) {
@@ -17,6 +18,9 @@ query ($id: String, $pid: String) {
     frontmatter {
       title
       areas {
+        flavor
+        callout
+        content
         name
         x
         y
@@ -39,9 +43,18 @@ query ($id: String, $pid: String) {
   }
   adventure: mdx(id: {eq: $pid}) {
     slug
+    frontmatter {
+      players {
+        class
+        name
+        passiveperception
+        race
+      }
+    }
   }
 }
 `
+const converter = new showdown.Converter();
 
 function Navigation(props) {
   const navItemsData = [
@@ -218,6 +231,69 @@ function GeneralFeatures(props) {
   );
 }
 
+function Areas(props) {
+  const areasList = props.areas.map((area, i) => {
+    const areaItem = (
+      <React.Fragment>
+        <AnchorLink
+          id={i+1}
+        />
+        <section>
+          {area.flavor &&
+            <div
+              dangerouslySetInnerHTML={{ __html: converter.makeHtml(area.flavor) }}
+            />
+          }
+          {area.callout &&
+            <div
+              dangerouslySetInnerHTML={{ __html: converter.makeHtml(area.callout) }}
+            />
+          }
+          <div
+            dangerouslySetInnerHTML={{ __html: converter.makeHtml(area.content) }}
+          />
+        </section>
+      </React.Fragment>
+    );
+    return (
+      <li>{areaItem}</li>
+    );
+  });
+
+  return (
+    <ol>
+      {areasList}
+    </ol>
+  );
+}
+
+function PlayerStats(props) {
+  const playersList = props.players.map((player, i) => {
+    const playersItem = (
+      <React.Fragment>
+        {player.name &&
+          <p>{player.name.split(' ')[0]}</p>
+        }
+        {player.passiveperception &&
+          <p>{player.passiveperception}</p>
+        }
+      </React.Fragment>
+    );
+    return (
+      <li key={i}>
+        {playersItem}
+      </li>
+    );
+  });
+  return (
+    <section>
+      <ul>
+        {playersList}
+      </ul>
+    </section>
+  );
+}
+
 class LocationPage extends React.Component {
   constructor(props) {
     super(props);
@@ -281,148 +357,19 @@ class LocationPage extends React.Component {
             body={this.props.data.mdx.body}
           />
         }
-        {/*<Areas></Areas>
-        <PlayerStats></PlayerStats>*/}
+        {this.props.data.mdx.frontmatter.areas &&
+          <Areas
+            areas={this.props.data.mdx.frontmatter.areas}
+          />
+        }
+        {this.props.data.adventure.frontmatter.players &&
+          <PlayerStats
+            players={this.props.data.adventure.frontmatter.players}
+          />
+        }
       </Layout>
     );
   }
 }
 
 export default LocationPage;
-
-/*function NavButtonsList(props) {
-  const navButtonsData = [
-    {
-      title: 'Map',
-      classes: ['iconfont', 'icon-map'].join(' ')
-    },
-    {
-      title: 'Features',
-      content: '&#9432;'
-    },
-    {
-      title: 'Up',
-      content: '&and;'
-    },
-    {
-      title: 'Down',
-      content: '&or;'
-    }
-  ];
-  const navButtonsMarkup = navButtonsData.map((b, i) => (
-    <NavigationButton
-      key={i}
-      data={b}
-    />
-  ));
-  return (
-    <nav>
-      <Link
-        to={props.location}
-      >
-        &lt;
-      </Link>
-      {navButtonsMarkup}
-    </nav>
-  );
-}
-
-function GroupStats(props) {
-  return (
-    <section>
-
-    </section>
-  );
-}
-
-class DiceRoller extends React.Component {
-  render() {
-    const amount = this.props.amount ? this.props.amount : '1',
-    type = this.props.type,
-    modifier = this.props.modifier ? (
-      this.props.modifier > 0 ? ('+' + this.props.modifier) : ('-' + this.props.modifier)
-    ) : '';
-    return (
-      <section>
-        <ul>
-          <li>
-            <p>{amount}d{type}{modifier}</p>
-          </li>
-          <li>
-            <p></p>
-          </li>
-        </ul>
-      </section>
-    );
-  }
-}
-
-class NavigationButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {anchor: ''}
-    this.hasNav = this.hashNav.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  hashNav() {
-    console.log(this.state.anchor);
-  }
-
-  handleClick(event) {
-    console.log(event.target);
-    this.setState({anchor: this.props.data.title}, this.hashNav);
-  }
-
-  render() {
-    return (
-      <button
-        onClick={this.handleClick} 
-        aria-label={'Navigation button: ' + this.props.data.title}
-        dangerouslySetInnerHTML={{ __html: this.props.data.content ? this.props.data.content : '&nbsp;' }}
-      />
-    );
-  }
-}
-
-class LocationPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dieType: 4,
-      dieAmount: 3,
-      modifier: 5,
-      result: 0
-    }
-  }
-
-  handleRoll(type, amount = 1, modifier = 0) {
-    const result = (1 + Math.floor(Math.random() * type)) * amount + modifier;
-    this.setState({result: result,})
-  }
-
-  render() {*/
-    /*
-    TODO: pass a prop to Layout containing a method defined in this class component that changes the states of the dice states defined above.
-
-    In the Layout component write code that looks for button formula text using a hook that fires after the page has loaded (componentDidMount()? maybe another). It will change text to buttons and give those buttons an onClick() method that references handleRoll
-    */
-    /*console.log(this.props.location.pathname.split('locations/')[0]);
-    return (
-      <Layout>
-        <NavButtonsList
-          location={this.props.location.pathname.split('/locations/')[0]}
-        />
-        <DiceRoller
-          type={this.state.dieType}
-          amount={this.state.dieAmount}
-          modifier={this.state.modifier}
-        />
-        <GroupStats/>
-        <h1>hello world</h1>
-      </Layout>
-    );
-  }
-}
-
-export default LocationPage;*/
