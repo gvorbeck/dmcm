f7523d57b91f32a9d53b904c00851de3303425a8
Garrett Vorbeck <me@iamgarrett.com>
2021-08-16 18:17:48 -0400

    changed diceroller component name

---
+++ src/components/dice/dice.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as styles from './dice.module.scss';
+
+class Dice extends React.Component {
+  render() {
+    const regex = new RegExp(/(\W*)(\d*d\d+\+?-?\d*)(\W*)/),
+    formula = regex.test(this.props.children) === true ? this.props.children.trim() : '???',
+    amount = formula.split('d')[0],
+    type = formula.split('d')[1].split(/[+-]/)[0],
+    modifier = formula.split(/\d*d\d+/)[1];
+    return (
+      <button
+        aria-label='Dice Roller Button'
+        className={styles.button + ' dmcm--dice-button'}
+        data-amount={amount}
+        data-type={type}
+        data-modifier={modifier}
+        data-formula={amount + 'd' + type + modifier}
+      >
+        {formula}
+      </button>
+    );
+  }
+}
+
+export default Dice;
