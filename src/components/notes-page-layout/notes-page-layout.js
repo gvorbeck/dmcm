a499567748c50b8f0e5bc7380af43cbb0ba9b2d8
Garrett Vorbeck <me@iamgarrett.com>
2021-08-17 10:55:03 -0400

    notes page content

--- src/components/notes-page-layout/notes-page-layout.js
+++ src/components/notes-page-layout/notes-page-layout.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Dice from '../dice/dice';
+import Layout from '../layout/layout';
+import { MDXProvider } from '@mdx-js/react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+
+export const query = graphql`
+  query ($id: String, $pid: String) {
+    mdx(id: {eq: $id}) {
+      id
+      body
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+    adventure: mdx(id: {eq: $pid}) {
+      slug
+    }
+  }
+`
+
+const shortcodes = { Dice };
 
 class NotesPage extends React.Component {
   render() {
     return (
-      <h1>hello</h1>
+      <Layout>
+        <Link to={`/${this.props.data.adventure.slug}`}>Back</Link>
+        <h1>{this.props.data.mdx.frontmatter.title}</h1>
+        <MDXProvider components={ shortcodes }>
+          <MDXRenderer>
+            {this.props.data.mdx.body}
+          </MDXRenderer>
+        </MDXProvider>
+      </Layout>
     );
   }
 }
