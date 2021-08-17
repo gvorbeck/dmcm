25c855c31c5beda824f38139984f61e5cd40bd4a
Garrett Vorbeck <me@iamgarrett.com>
2021-08-17 15:28:22 -0400

    added static query to header module

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, StaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Navigation from '../navigation/navigation';
+import * as styles from './header.module.scss';
 
 function Header(props) {
   return (
-    <header>
-      <h1>
-        <Link to='/'>Dungeon Master's Campaign Manager</Link>
-      </h1>
-      {!props.homeNav &&
-        <Navigation/>
-      }
-    </header>
+    <StaticQuery
+      query={graphql`
+        query HeaderQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <header
+          className={styles.header}
+        >
+          <StaticImage
+            src='../../images/wizard.png'
+            alt={data.site.siteMetadata.title}
+            placeholder='blurred'
+            layout='fixed'
+            width={10}
+          />
+          <h1>
+            <Link to='/'>{data.site.siteMetadata.title}</Link>
+          </h1>
+          {!props.homeNav &&
+            <Navigation/>
+          }
+        </header>
+      )}
+    />
   );
 }
 
