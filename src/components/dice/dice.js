92d6c0f8e7e28707fb6e08c0e7f4d1997f3537ee
Garrett Vorbeck <me@iamgarrett.com>
2021-09-07 14:33:32 -0400

    more versatile formula isolation

--- src/components/dice/dice.js
+++ src/components/dice/dice.js
@@ -3,15 +3,20 @@ import * as styles from './dice.module.scss';
 
 class Dice extends React.Component {
   render() {
-    const regex = new RegExp(/(\W*)(\d*d\d+\+?-?\d*)(\W*)/),
-    formula = regex.test(this.props.children) === true ? JSON.stringify(this.props.children).replace(/"]|[["]/g, '') : '???',
-    amount = formula.split('d')[0],
+    let formula;
+    const regex = new RegExp(/(\W*)(\d*d\d+\+?-?\d*)(\W*)/);
+    if (typeof this.props.children === 'string') {
+      formula = regex.test(this.props.children) === true ? this.props.children.trim() : '???';
+    } else {
+      formula = regex.test(this.props.children[0]) === true ? this.props.children[0].trim() : '???';
+    }
+    const amount = formula.split('d')[0],
     type = formula.split('d')[1].split(/[+-]/)[0],
     modifier = formula.split(/\d*d\d+/)[1];
     return (
       <button
         aria-label='Dice Roller Button'
-        className={styles.button + ' dmcm--dice-button'}
+        className={`${styles.button} dmcm--dice-button`}
         data-amount={amount}
         data-type={type}
         data-modifier={modifier}
