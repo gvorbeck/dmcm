0930f1150b7dec132cd11438852f56f090e09788
Garrett Vorbeck <me@iamgarrett.com>
2021-09-07 14:37:59 -0400

    added source

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import "@fontsource/space-grotesk";
 import "@fontsource/ibm-plex-sans";
+/* refer to https://seiyria.com/gameicons-font/ for icon names */
 import "./src/styles/game-icons.scss";
 import "./src/styles/global.scss";
