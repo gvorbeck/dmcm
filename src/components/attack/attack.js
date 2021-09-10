1ea943961c3e1cfa617e98998bbb527f28e99d43
Garrett Vorbeck <me@iamgarrett.com>
2021-09-10 18:45:01 -0400

    button with dataset and children setup

--- src/components/attack/attack.js
+++ src/components/attack/attack.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 class Attack extends React.Component {
   render() {
+    const raw = this.props.children.join('').split('|'),
+    attack = {
+      name: raw[0],
+      tohit: raw[1],
+      formula: raw[2],
+      type: raw[3]
+    };
+
+    console.log(raw);
     return (
-      <button>boop</button>
+      <button
+        aria-label='Attack Button'
+        className={`dmcm--attack-button`}
+        data-name={attack.name}
+        data-tohit={attack.tohit}
+        data-formula={attack.formula}
+        data-type={attack.type}
+      >
+        {attack.name}
+      </button>
     );
   }
 }
